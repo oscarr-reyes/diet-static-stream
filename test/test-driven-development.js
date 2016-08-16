6d2d492dbb590892e3f79d74ed6afb43320bbf17
Nosthertus <nosthertus@gmail.com>
2016-08-16 16:37:40 -0400

    ADDED: Test-driven-development file
    *Added file for test-driven-development

---
+++ test/test-driven-development.js
@@ -0,0 +1,87 @@
+var expect = require("expect");
+var helper = require("./../lib/helper.js");
+var file   = require("./../lib/file.js");
+var path   = require("path");
+
+/*
+ * Define CONST options for file.js
+ */
+module.CONST = {
+	OPTIONS: helper.parseOptions({
+		path: "test/static"
+	})
+};
+
+describe("helper functions", function(){
+	it("should parse options", function(){
+		expect(function(){
+			helper.parseOptions();
+		}).toThrow("Options is undefined");
+
+		expect(function(){
+			helper.parseOptions([]);
+		}).toThrow("Options parameter must be an object");
+
+		expect(function(){
+			helper.parseOptions({});
+		}).toThrow("Path for static files is undefined");
+
+		expect(helper.parseOptions({path: "static"})).toIncludeKeys([
+			"path",
+			"index",
+			"defaultExtension",
+			"cache",
+			"scriptName"
+		]);
+	});
+
+	it("should set extension", function(){
+		// Do not set extension cause parameter extension is undefined
+		expect(helper.setExtension("path/to/file")).toBe("path/to/file");
+
+		// Set extension to path without extension
+		expect(helper.setExtension("path/to/file", "md")).toBe("path/to/file.md");
+		
+		// Change extension to path with extension
+		expect(helper.setExtension("path/to/file.js", "md")).toBe("path/to/file.md");
+	});
+
+	it("should resolve index", function(){
+		expect(function(){
+			helper.resolveIndex([]);
+		}).toThrow("Index value not valid");
+
+		expect(helper.resolveIndex(true)).toBe("index");
+		expect(helper.resolveIndex(false)).toBe(null);
+		expect(helper.resolveIndex("file")).toBe("file");
+	});
+});
+
+describe("file functions", function(){
+	it("should parse directory", function(){
+		// The directory found is not a file
+		expect(file.parseDir("path/to/file")).toInclude({file: false});
+		
+		// The directory found is a file
+		expect(file.parseDir("path/to/file.js")).toInclude({file: true});
+	});
+
+	it("should find file", function(){
+		var dir = module.CONST.OPTIONS.path;
+
+		// When a file is not found
+		expect(file.findFile("path/to/file")).toBe(null);
+
+		// When no extension was given in url
+		expect(file.findFile(dir)).toInclude({
+			file: true,
+			dir: path.join(dir, "index.html")
+		});
+
+		// When extension was given in url
+		expect(file.findFile(path.join(dir, "index.html"))).toInclude({
+			file: true,
+			dir: path.join(dir, "index.html")
+		});
+	});
+});
